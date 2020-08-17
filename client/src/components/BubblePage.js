bbaa3827c8e07c5957d48c364dc35d478abd989a
Nico Chikuji <chiku524@icloud.com>
2020-08-16 22:51:48 -0400

    the styling is affecting the edit and delete button for some reason, but mvp is finished and working! just had to comment out some styles

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,19 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../api/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  axiosWithAuth().get('http://localhost:5000/api/colors')
+        .then(response => {
+          console.log(response);
+          setColorList(response.data);
+        })
+
 
   return (
     <>
